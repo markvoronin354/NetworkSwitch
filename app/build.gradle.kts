7f9ba4c3a6ca2b39edf80615de183d3596fabedf
markvoronin354 <markvoronin354@gmail.com>
2026-09-22 13:49:15 -0400

    v2.5.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.supernova.networkswitch"
         minSdk = 29
         targetSdk = 35
-        versionCode = 210
-        versionName = "2.1.0"
+        versionCode = 250
+        versionName = "2.5.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         
